5a4d5e4c62d3e1e2fac04bcc445410d42a40994e
DriRSantos <drirsantos@outlook.com>
2023-06-11 11:03:06 -0300

    adding id_categoria on CategoriaDTO

--- src/main/java/com/trabalhoFinal/apiEcommerce/dto/CategoriaDTO.java
+++ src/main/java/com/trabalhoFinal/apiEcommerce/dto/CategoriaDTO.java
@@ -4,10 +4,17 @@ import java.util.List;
 
 public class CategoriaDTO {
 
+	private Integer id_categoria;
 	private String nome;
 	private String descricao;
 	private List<ProdutoCatDTO> produtos;
 	
+	public Integer getId_categoria() {
+		return id_categoria;
+	}
+	public void setId_categoria(Integer id_categoria) {
+		this.id_categoria = id_categoria;
+	}
 	public String getNome() {
 		return nome;
 	}
